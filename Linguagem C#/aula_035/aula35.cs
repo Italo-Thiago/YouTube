08facef9e47fdd9058d6dc0b83901a42c68b8226
Italo Thiago <italothiago220@gmail.com>
2023-10-19 21:26:33 -0300

    19/10 finalização aula35

--- Linguagem C#/aula_035/aula35.cs
+++ Linguagem C#/aula_035/aula35.cs
@@ -23,6 +23,15 @@ class Veiculo
   public int getWheels(){
     return wheels;
   }
+  public void setWheels(int wheels){
+    if(wheels<0){
+      this.wheels=0;
+    }else if(wheels>40){
+      this.wheels=40;
+    }else{
+      this.wheels=wheels;
+    }
+  }
 }
 
 class Carro:Veiculo
@@ -38,17 +47,36 @@ class Carro:Veiculo
   }
 }
 
+class CarroCombate:Carro{
+  public int municao;
+  public CarroCombate():base("Carro de Combate","Verde"){
+    municao=100;
+    setWheels(6);
+  }
+}
+
 class Program{
 
 	static void Main(){
-    Carro c1=new Carro("Fast","red");
+    Carro c1=new Carro("Fast","Red");
+    CarroCombate cc1=new CarroCombate();
 
     c1.ligar();
 
-    Console.WriteLine("Cor........:{0}",c1.color);
-    Console.WriteLine("Nome.......:{0}",c1.name);
-    Console.WriteLine("Rodas......:{0}",c1.getWheels());
-    Console.WriteLine("Vel.Máxima.:{0}",c1.velMax);
-    Console.WriteLine("Ligado.....:{0}",c1.getLigado());
+    Console.WriteLine("Cor........: {0}",c1.color);
+    Console.WriteLine("Nome.......: {0}",c1.name);
+    Console.WriteLine("Rodas......: {0}",c1.getWheels());
+    Console.WriteLine("Vel.Máxima.: {0}",c1.velMax);
+    Console.WriteLine("Ligado.....: {0}",c1.getLigado());
+
+    Console.WriteLine("------------------------------------");
+    
+    Console.WriteLine("Cor........: {0}",cc1.color);
+    Console.WriteLine("Nome.......: {0}",cc1.name);
+    Console.WriteLine("Rodas......: {0}",cc1.getWheels());
+    Console.WriteLine("Vel.Máxima.: {0}",cc1.velMax);
+    Console.WriteLine("Ligado.....: {0}",cc1.getLigado());
+    Console.WriteLine("Munição....: {0}",cc1.municao);
+	
 	}
 }
